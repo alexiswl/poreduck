7541e3d972e40a6634bab77993d8209ddac307eb
Alexis <alexiswl@student.unimelb.edu.au>
2017-04-05 16:02:18 +1000

    Almost done, just the rsync command left

--- transfer_fast5_to_server.py
+++ transfer_fast5_to_server.py
@@ -30,7 +30,7 @@ import sys  # For stopping in the event of errors.
 import subprocess  # Running rsync and tar functions.
 import argparse  # Allow users to set commandline arguments and show help
 import getpass  # Prompts user for password, just a oneoff to runt he script.
-from pexpect import pxssh  # Connecting via ssh to make sure that the parent of the destination folder is there.
+from pexpect import pxssh, spawn  # Connecting via ssh to make sure that the parent of the destination folder is there.
 import time  # For snoozing
 import pandas as pd  # Create dataframe of list of files with attributes for each.
 
@@ -43,7 +43,7 @@ PASSWORD = ""
 DEST_DIRECTORY = ""
 TIMEOUT = 1800
 CSV_DIR = ""
-
+RSYNC_SUBPROCESS = ""
 
 def main():
     # Get argument list, password for server and set directories.
@@ -57,9 +57,10 @@ def main():
 
     # While loop to continue tarring up folders
     while rsync_running:
+        os.chdir(READS_DIR)
         # Get list of sub-directories
         subdirs = get_subdirs()
-
+        print(subdirs)
         # May not be any new folders
         if len(subdirs) == 0:
             have_a_break()
@@ -70,11 +71,15 @@ def main():
             # Is folder finished?
             folder_status = check_folder_status(subdir)
             if folder_status == "still writing":
-                continue
-
+               next 
+	    print(folder_status)
             # Tar up folder(s)
-            tar_folders(subdir)
-
+            tar_folders(subdir.split("/")[-2])
+        
+        if RSYNC_SUBPROCESS.poll() is not None:
+           stdout, stderr = RSYNC_SUBPROCESS.communicate()
+           print(stdout, stderr)
+           rsync_running = False 
     # Now we need to tar up the last folder.
 
 
@@ -119,7 +124,7 @@ def check_directories():
     os.chdir(READS_DIR)
 
     # Create CSV directory if it does not exist
-    CSV_DIR = READS_DIR + "csv"
+    CSV_DIR = READS_DIR + "csv/"
     if not os.path.isdir(CSV_DIR):
         os.mkdir(CSV_DIR)
 
@@ -153,26 +158,26 @@ def check_directories():
 
 
 def run_rsync_command():
-    # Create password file:
-    pwd_filename = "~/.minion_transfer_pwd.txt"
-    subprocess.call("touch %s" % pwd_filename, shell=True)  # Create the password
-    subprocess.call("chmod 600 %s" % pwd_filename, shell=True)  # Reduce the permissions so only you can read/write to it.
-    subprocess.call("echo %s > %s" % (PASSWORD, pwd_filename), shell=True)
-
+    global RSYNC_COMMAND
     # Generate list of rsync options to be used.
     rsync_command_options = []
     # Free space on the laptop by deleting files that have been transferred
     rsync_command_options.append("--remove-source-files")
-    rsync_command_options.append("--timeout=%d" % TIMEOUT)
-    rsync_command_options.append("--password-file=%s" % pwd_filename)
+    rsync_command_options.append("--timeout=%d" % TIMEOUT) 
+    rsync_command_options.append("--include=*.tar.gz")
 
     # Using the 'rsync [OPTION]... SRC [SRC]... [USER@]HOST:DEST' permutation of the command
-    rsync_command = "rsync %s %s*.tar.gz %s@%s:%s" % (' '.join(rsync_command_options),
-                                                      READS_DIR,
+    rsync_command = "sshpass -p %s rsync %s %s %s@%s:%s" % (
+                                                      PASSWORD, 
+                                                      ' '.join(rsync_command_options),
+						      READS_DIR, 
                                                       SERVER_USERNAME,
                                                       SERVER_NAME,
                                                       DEST_DIRECTORY)
-    subprocess.Popen(rsync_command, shell=True)
+    
+    RSYNC_COMMAND = subprocess.Popen(rsync_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+   
+  
 
 
 def get_subdirs():
@@ -180,14 +185,19 @@ def get_subdirs():
                if os.path.isdir(directory)  # Make sure that the subdirectory is a directory
                and not directory == "tmp"   # And not the tmp directory
                and not directory == "csv"]  # And not our csv directory that we'll create.
-
+    
+    subdirs_keep = []
     # Remove those that are not ints.
-    for subdir in subdirs:
+    for subdir in subdirs: 
+        sub_int = subdir.split("/")[-2] 
         try:
-            int(subdir)
-            return True
+            int(sub_int) 
+            print("%s is an int" % sub_int)
+	    subdirs_keep.append(subdir)
         except ValueError:
-            subdirs.remove(subdir)
+            print("%s is not our folder" % subdir)
+
+    subdirs = subdirs_keep
 
     # Sort subdirectories by writing time.
     return sorted(subdirs, key=os.path.getmtime)
@@ -227,13 +237,15 @@ def check_folder_status(subdir):
 
     for run in runs:
         # Before moving the mux files we need to make sure that there is some sequencing run files in the folder
-        if len(fast5_pd.loc[(fast5_pd.rnumber == run) & (fast5_pd.mux == "NO")]) == 0:
+        if len(fast5_pd.loc[(fast5_pd.mux == "NO")]) == 0:
             continue  # No sequencing run files in the folder, skipping folder.
 
         # Move mux scan files for a given run
         fast5_to_move_pd = fast5_pd.loc[(fast5_pd.rnumber == run) & (fast5_pd.mux == "YES")]
         fast5_to_move = fast5_to_move_pd.index.values  # The row names equal the names of the files.
+        print("Number of mux files to move is %s for %s" % (len(fast5_to_move), subdir))
         if len(fast5_to_move) != 0:  # Something here, let's move!!
+            print("We have mux files!")
             is_mux = True
             return_status = "moving files"
             move_fast5_files(subdir, fast5_to_move, run, is_mux)
@@ -242,14 +254,22 @@ def check_folder_status(subdir):
         # Move standard sequencing run files for a given run
         fast5_to_move_pd = fast5_pd.loc[(fast5_pd.rnumber == run) & (fast5_pd.mux == "NO")]
         fast5_to_move = fast5_to_move_pd.index.values  # The row names equal the names of the files.
-
+       
+        print(len(fast5_to_move))
         # Before moving the sequencing run files, we need to make sure that this the number of fast5 files is 4000
-        if len(fast5_to_move) == 4000:
-            return_status = "moving_files"
+        if len(fast5_to_move) >= 4000:
+            return_status = "moving files"
             is_mux = False
             move_fast5_files(subdir, fast5_to_move, run, is_mux)
             fast5_to_move_pd.to_csv(CSV_DIR + subdir.split("/")[-2] + "_" + run)
-
+        
+    # Check if folder is empty
+    fast5_files = [fast5_file for fast5_file in os.listdir(subdir)
+                   if fast5_file.endswith(".fast5")]
+    if len(fast5_files) == 0:
+       subprocess.call("rm -r %s" % subdir, shell=True)
+    
+    os.chdir(READS_DIR)  # Return to the reads directory
     return return_status  # Used for if we bother trying to tar up in the next step.
 
 
@@ -266,15 +286,16 @@ def move_fast5_files(subdir, fast5_files, run, is_mux):
     os.mkdir(new_dir)
 
     for fast5_file in fast5_files:
-        subprocess.call("mv %s %s" % (fast5_file, new_dir))
+        subprocess.call("mv %s %s" % (fast5_file, new_dir), shell=True)
 
 
 def tar_folders(subdir_prefix):
     # Get the subdirectories that start with the initial subdirectory.
     # So 0 may now be 0_12345 where 12345 is the rnumber.
-    subdirs = [subdir for subdir in get_subdirs()
+    print(subdir_prefix)
+    subdirs = [subdir for subdir in os.listdir(READS_DIR)
                if subdir.startswith(subdir_prefix + "_")]
-
+    print(subdirs)
     # Now tar up each folder individually
     for subdir in subdirs:
         tar_command = "tar -cf - %s | pigz -9 -p 32 > %s.tar.gz" % (subdir, subdir)
