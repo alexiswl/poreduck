6fdde55b2d55e4d77c339ca2d2f2ed9a4cffcdd0
alexiswl <awlucattini@icloud.com>
2017-05-22 09:52:25 +1000

    turned archive into boolean store-true command

--- move_fast5_to_subfolders.py
+++ move_fast5_to_subfolders.py
@@ -25,7 +25,7 @@ def get_args():
     parser = argparse.ArgumentParser(description="Moves fast5 files from folder into subdirectories")
     parser.add_argument("--reads_dir", type=str, required=True,
                         help="The directory with all the fast5 files in them")
-    parser.add_argument("--archive", default=False, dest='archive',
+    parser.add_argument("--archive", default=False, dest='archive', action='store_true',
                         help="Zip up folder once complete.")
     parser.add_argument("--num_threads", default=5,
                         help="Number of mv commands we will allow to be running at any given time.")
@@ -41,7 +41,7 @@ def set_directories(args):
     READS_DIR = args.reads_dir
     if not os.path.isdir(READS_DIR):
         sys.exit("%s not a directory" % READS_DIR)
-    os.path.abspath(READS_DIR) + "/"
+    READS_DIR = os.path.abspath(READS_DIR) + "/"
     os.chdir(READS_DIR)
 
 
@@ -54,16 +54,17 @@ def move_fast5_files(args):
     fast5_df = pd.DataFrame(columns=['fast5_file', 'subfolder', 'mv_command'])
 
     fast5_df['fast5_file'] = [fast5_file for fast5_file in os.listdir(READS_DIR)]
-    fast5_df['subfolder'] = [int(i / 4000) for i in xrange(len(fast5_df))]
+    fast5_df['subfolder'] = [standardise_int_length(int(i / 4000)) for i in xrange(len(fast5_df))]
     fast5_df['mv_command'] = ["mv %s %s" % (fast5_file, subfolder)
                               for fast5_file, subfolder in izip(fast5_df.fast5_file, fast5_df.subfolder)]
 
-    subdirectories = fast5_df.subfolder.unique()
+    subdirectories = fast5_df.subfolder.unique().tolist()
+    print(subdirectories)
     for subdirectory in subdirectories:
         os.mkdir(subdirectory)
 
     processes = (subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                 for cmd in fast5_df.mv_command.to_list())
+                 for cmd in fast5_df.mv_command.tolist())
 
     # We use the islice command to split our list of mv commands into five smaller lists.
     running_processes = list(itertools.islice(processes, args.num_threads))
@@ -94,13 +95,32 @@ def archive_folders(args, directory_list):
     # Otherwise a simple tar command should do
     tar_commands = []
     for directory in directory_list:
-        tar_commands.append("tar -cf - %s --remove-files | pigz -9 -p 16 > %s.tar.gz" %
-                            directory, directory)
+        tar_commands.append("tar -cf - %s --remove-files | pigz -9 -p 8 > %s.tar.gz" %
+                            (directory, directory))
 
-    # Now use the subprocess command to tar up each of the files
-    for tar_command in tar_commands:
-        tar_proc = subprocess.Popen(tar_command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        stdout, stderr = tar_proc.communicate()
+    # Multi-thread our tar command
+    processes = (subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                  for cmd in tar_commands)
+
+    # We use the islice command to split our  commands into five smaller lists.
+    running_processes = list(itertools.islice(processes, args.num_threads))
+
+    while running_processes:
+        for i, process in enumerate(running_processes):
+            if process.poll() is not None:  # Means that the process is complete!
+                stdout, stderr = process.communicate()  # Get the output of the completed process
+                if not stderr == "":
+			print stderr 
+                running_processes[i] = next(processes, None)
+                # Run the next number in the list.
+                if running_processes[i] is None:  # No more commands waiting to be processed.
+                   del running_processes[i]  # Not a valid process.
+                   break 
+
+
+def standardise_int_length(my_integer):
+    # Input of 15 returns 0015
+    return "%04d" % int(my_integer)
 
 
 # Run the main function.
