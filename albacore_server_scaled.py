e1f1a16a13fd70ed79ff11eef411482119cdc09a
Alexis <alexiswl@student.unimelb.edu.au>
2017-05-30 18:47:48 +1000

    Another revamp of the albacore_server_scaled script. Allows for re-runs and checks using qacct to determine if a given job has failed, completed or commenced.

--- albacore_server_scaled.py
+++ albacore_server_scaled.py
@@ -293,7 +293,7 @@ def pick_up_from_previous_run():
             SUBFOLDERS[subfolder].albacore_complete = row['albacore_complete']
             SUBFOLDERS[subfolder].folder_removed = row['folder_removed']
             SUBFOLDERS[subfolder].fastq_moved = row['fastq_moved']
-            # Now make sure none of the jobs have failed
+            # Now make sure none of the jobs have failed and reset if so
             if has_failed(SUBFOLDERS[subfolder].albacore_jobid):
                 SUBFOLDERS[subfolder].albacore_submitted = False
                 SUBFOLDERS[subfolder].albacore_commenced = False
