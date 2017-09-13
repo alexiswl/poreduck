dd3e3b9445017b8b2fac2b2215e319db2dbd2f08
alexiswl <alexiswl@student.unimelb.edu.au>
2017-09-13 22:22:20 +1000

    mux and duration were excluded, preventing channel map from being created.

--- poreduck/plot_yields.py
+++ poreduck/plot_yields.py
@@ -135,6 +135,10 @@ class Read_Set:
             channel_csv = csv_row.channel
             read_csv = csv_row.read_no
             df_index = self.df.query("channel==@channel_csv & read==@read_csv").index
+            # Write value to dictionary with index of our fastq dataframe as the key
+            muxs[df_index] = csv_row.mux
+            duration[df_index] = csv_row.duration
+        # Now write the value of the mux and duration to fastq dataframe
         for index, mux in muxs.items():
             self.df.set_value(index, "mux", mux)
         for index, duration in durations.items():
@@ -258,7 +262,7 @@ def plot_yield_general():
     ax.set_title(f"Yield for {SAMPLE_NAME} (MB/Hour)")
     ax.plot(YIELD_DATA['duration_float'], YIELD_DATA['cumsum_bp'],
             linestyle="solid", markevery=[])
-    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_general_yield_plot.png"))
+    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_yield_plot.png"))
 
 
 def plot_yield_by_quality():
@@ -312,7 +316,7 @@ def plot_yield_by_quality():
     ax.yaxis.set_major_formatter(formatter_y)
     ax.legend([mpatches.Patch(color=colour) for colour in reversed(QUALITY_COLOURS)],
               reversed(QUALITY_DESCRIPTIONS), loc=2)
-    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_stacked_yield_plot.png"))
+    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_yield_plot_by_quality.png"))
 
 
 def plot_read_length_hist():
@@ -343,10 +347,10 @@ def plot_read_length_hist():
     ax.grid(color='black', linestyle=':', linewidth=0.5)
     ax.set_xlabel("Read length")
     ax.set_ylabel("Bases per bin")
-    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_read_length_hist.png"))
+    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_hist_read_length_by_basepair.png"))
 
 
-def plot_heatmap():
+def plot_poremap():
     def minknow_column_order(i):
         return chain(range(i + 33, i + 41), reversed(range(i + 1, i + 9)))
     # Channels are not in order, 121 is in the topleft, 89 in the top right.
@@ -400,8 +404,8 @@ def plot_heatmap():
     # Create line down the middle as shown in MinKNOW.
     ax.axvline([8], color='white', lw=15)
     # Nice big title!
-    ax.set_title("Heatmap of Yield by Channel", fontsize=25);
-    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_heatmap.png"))
+    ax.set_title("Map of Yield by Channel", fontsize=25);
+    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_yield_map_by_pore.png"))
 
 
 def plot_pore_yield_hist():
@@ -424,6 +428,7 @@ def plot_pore_yield_hist():
     ax.grid(color='black', linestyle=':', linewidth=0.5)
     ax.set_xlabel("Yield in single pore")
     ax.set_ylabel("Pores per bin")
+    savefig(os.path.join(PLOTS_DIR, f"{SAMPLE_NAME}_hist_yield_by_pore.png"))
 
 
 def y_hist_to_human_readable(y, position):
@@ -479,7 +484,7 @@ def run_plot_commands():
     plot_yield_general()
     plot_yield_by_quality()
     plot_read_length_hist()
-    plot_heatmap()
+    plot_poremap()
     # CSV specific plots
     if not CSV_DIR == "":
         plot_pore_yield_hist()
