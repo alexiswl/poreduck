0e33d3332f6fdabf1be170ce5be0e7eeb4306e4e
alexiswl <alexiswl@student.unimelb.edu.au>
2017-10-12 12:44:02 +1100

    Read length histogram now also overlapping

--- poreduck/compare_runs.py
+++ poreduck/compare_runs.py
@@ -134,7 +134,28 @@ def plot_read_length_hist():
 
     # Ensure labels are not missed.
     plt.tight_layout()
-    savefig(os.path.join(PLOTS_DIR, f"{plot_prefix}_read_length_hist.png"))
+    savefig(os.path.join(PLOTS_DIR, f"{plot_prefix}_read_length_hist.pyjoy.png"))
+
+    # Plot the histogram using plt.plot
+    # Close any previous plots
+    plt.close('all')
+    # Set subplots.
+    fig, ax = plt.subplots(1)
+    for run, seq_df in zip(RUNS, SEQ_DFS):
+        # Plot each yield plot through a for loop.
+        """For loop here with SEQ_DFS here"""
+        ax.hist(seq_df, label=run.name, bins=50)
+    # Set the axis formatters
+    ax.xaxis.set_major_formatter(FuncFormatter(x_hist_to_human_readable))
+    # Set the titles and add a legend.
+    title_string = ", ".join([name for name in NAMES[:-1]]) + " and " + NAMES[-1]
+    ax.set_title(f"Read Distribution Graph for {title_string}")
+    ax.grid(color='black', linestyle=':', linewidth=0.7)
+    """Need to have another 'regex' name"""
+    plot_prefix = '_'.join([name.replace(" ","_") for name in NAMES])
+    # Ensure labels are not missed.
+    fig.tight_layout()
+    savefig(os.path.join(PLOTS_DIR, f"{plot_prefix}_read_length_hist.overlap.png"))
 
 
 def plot_yield_general():
