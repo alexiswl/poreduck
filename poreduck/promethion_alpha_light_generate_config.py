dbfdeef21fb0a900f3a8afd738864ce8ca4b772b
Alexis Lucattini <alexiswl@student.unimelb.edu.au>
2018-10-10 12:18:21 +1100

    Finished testing of config file

--- poreduck/promethion_alpha_light_generate_config.py
+++ poreduck/promethion_alpha_light_generate_config.py
@@ -5,9 +5,9 @@ import re
 import pandas as pd
 import json
 
-"""
-Generate a yaml file used to run each of the alpha_light python commands
-"""
+#"""
+#Generate a yaml file used to run each of the alpha_light python commands
+#"""
 
 
 def get_args():
@@ -52,17 +52,22 @@ def get_all_files(sequencing_summary_dir, fastq_dir, fast5_dir):
                   if os.path.isdir(os.path.join(fast5_dir, fast5_folder))
                   and re.match("^\d+$", fast5_folder)]
 
-    sequencing_summary_df = pd.DataFrame(sequencing_summary_files, columns="sequencing_summary_file")
-    fastq_df = pd.DataFrame(fastq_files, columns="fastq_file")
-    fast5_df = pd.DataFrame(fast5_dirs, columns="fast5_dirs")
+    sequencing_summary_df = pd.DataFrame(sequencing_summary_files, columns=["sequencing_summary_file"])
+    fastq_df = pd.DataFrame(fastq_files, columns=["fastq_file"])
+    fast5_df = pd.DataFrame(fast5_dirs, columns=["fast5_dir"])
 
     # Append number onto each dataframe
     sequencing_summary_df['number'] = sequencing_summary_df['sequencing_summary_file'].apply(
-        lambda x: re.match("sequencing_summary_(\d+).txt", x).group(1))
+        lambda x: int(re.match("sequencing_summary_(\d+).txt", os.path.basename(x)).group(1)))
     fastq_df['number'] = fastq_df['fastq_file'].apply(
-        lambda x: re.match("fastq_(\d+).fastq", x).group(1))
-    fast5_df['number'] = fast5_df['fastq_file'].apply(
-        lambda x: re.match('(\d+)', x).group(1))
+        lambda x: int(re.match("fastq_(\d+).fastq", os.path.basename(x)).group(1)))
+    fast5_df['number'] = fast5_df['fast5_dir'].apply(
+        lambda x: int(re.match('(\d+)', os.path.basename(x)).group(1)))
+
+    # Sort dataframes by number
+    sequencing_summary_df.sort_values(by=['number'], inplace=True)
+    fastq_df.sort_values(by=['number'], inplace=True)
+    fast5_df.sort_values(by=['number'], inplace=True)
 
     # Set number as index for each dataframe
     sequencing_summary_df.set_index("number", inplace=True)
@@ -70,12 +75,12 @@ def get_all_files(sequencing_summary_dir, fastq_dir, fast5_dir):
     fast5_df.set_index("number", inplace=True)
 
     # Now merge each using pd.concat
-    return pd.concat(sequencing_summary_df, fastq_df, fast5_df)
+    return pd.concat([sequencing_summary_df, fastq_df, fast5_df], axis='columns', join='inner', sort=True)
 
 
 def output_yaml(yaml_file, dataset):
     with open(yaml_file, 'w') as file:
-        yaml.dump(json.loads(dataset.to_json()), file, default_flow_style=True)
+        yaml.dump(json.loads(dataset.to_json(orient='records')), file, default_flow_style=True)
 
 
 def main():
@@ -94,7 +99,7 @@ def main():
     dataset['md5_fastq'] = args.output_md5sum_fastq
 
     # Output the yaml file
-    output_yaml(args.yaml_file, dataset)
+    output_yaml(args.output_yaml_file, dataset)
 
 
 if __name__ == "__main__":
