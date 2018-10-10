87408ed479d4aca1a13f765476552a6206ca0a96
Alexis Lucattini <alexiswl@student.unimelb.edu.au>
2018-10-10 12:18:45 +1100

    Finished testing of wrapper file

--- poreduck/promethion_alpha_light_wrapper.py
+++ poreduck/promethion_alpha_light_wrapper.py
@@ -6,6 +6,7 @@ import json
 import pandas as pd
 import subprocess
 import logging
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -14,6 +15,7 @@ def get_args():
     parser = argparse.ArgumentParser(description="Get the a yaml file created in the config script")
     parser.add_argument("--config", required=True,
                         help="Path to config file")
+    return parser.parse_args()
 
 
 def read_config(config):
@@ -23,15 +25,16 @@ def read_config(config):
 
 
 def run_process(config_data):
-    tar_proc = subprocess.run(["promethion_alpha_light.py",  # Then come the options.
-                               "--sequencing_summary_path=%s" % config_data["sequencing_summary_file"],
-                               "--fastq_path=%s" % config_data['fastq_file'],
-                               "--fast5_path=%s" % config_data['fast5_file'],
-                               "--flowcellID=%s" % config_data['FlowcellID'],
-                               "--rnumber=%s" % config_data["rnumber"],
-                               "--md5_fast5=%s" % config_data['md5_fast5'],
-                               "--md5_fastq=%s" % config_data['md5_fastq'],
-                               "--dry-run"], #"--inplace"],
+    here = os.path.dirname(os.path.realpath(__file__))
+    tar_proc = subprocess.run([os.path.join(here, "promethion_alpha_light.py"),  # Then come the options.
+                               "--sequencing_summary_path=%s" % config_data.sequencing_summary_file,
+                               "--fastq_path=%s" % config_data.fastq_file,
+                               "--fast5_path=%s" % config_data.fast5_dir,
+                               "--flowcellID=%s" % config_data.FlowcellID,
+                               "--rnumber=%s" % config_data.rnumber,
+                               "--md5_fast5=%s" % config_data.md5_fast5,
+                               "--md5_fastq=%s" % config_data.md5_fastq,
+                               "--inplace"],
                               capture_output=True)
 
     if tar_proc.returncode == 0:
@@ -44,3 +47,13 @@ def run_process(config_data):
         logging.warning("Stderr = %s" % tar_proc.stderr.decode())
 
 
+def main():
+    args = get_args()
+    dataframe = pd.DataFrame(read_config(args.config))
+
+    for row in dataframe.itertuples():
+        run_process(row)
+
+
+if __name__ == "__main__":
+    main()    
